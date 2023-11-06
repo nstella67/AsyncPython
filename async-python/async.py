272254bf3335f461e57c3336f9901fbb3c48abf0
Nuri Lee <leenuri67@gmail.com>
2023-11-06 16:59:43 +0900

    first commit

---
+++ async-python/async.py
@@ -0,0 +1,39 @@
+import time
+import asyncio
+
+
+async def delivery(name, mealtime):
+    print(f"{name}에게 배달 완료!")
+    await asyncio.sleep(mealtime)
+    print(f"{name} 식사 완료, {mealtime}시간 소요...")
+    print(f"{name} 그릇 수거 완료")
+    return mealtime
+
+
+async def main():
+
+    await asyncio.gather(
+        delivery("A", 5),
+        delivery("B", 3),
+        delivery("C", 4),
+    )
+    # result = await asyncio.gather(
+    #     delivery("A", 1),
+    #     delivery("B", 2),
+    #     delivery("C", 3),
+    # )
+
+    # print(result)
+    
+    #sync.py와 같음
+    await delivery("A", 5)
+    await delivery("B", 3)
+    await delivery("C", 4)
+    # await을 기점으로 동기적으로 처리
+
+
+if __name__ == "__main__":
+    start = time.time()
+    asyncio.run(main())
+    end = time.time()
+    print(end - start)
---
+++ hello.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+print(app)
+
+# def main():
+#     ''' dfhdfhdf '''
+#     print('hello')
+
+# print("hello world!")
